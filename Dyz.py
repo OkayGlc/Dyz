a9525504de7beea6f65d0b030c3a1519404792fd
OkayGlc <59234932+OkayGlc@users.noreply.github.com>
2020-01-07 17:13:28 +0300

    Update Dyz.py

--- Dyz.py
+++ Dyz.py
@@ -5,18 +5,15 @@ print ("Dyz2 kullanıma hazır")
 
 print ("Ne yapmami istersiniz?")
 
-while True :
+
 
 istek = input("&&:")
 
-break
+
 
 if (istek=="hack"):
   
   print ("sucsesful")
-
-
-elif (istek=="diger"):
 
 
 else :
